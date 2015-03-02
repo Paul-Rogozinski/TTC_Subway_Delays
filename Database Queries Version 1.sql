6490e93ce9886d387db787ea5516a5e43a6a2cd1
SHIVANI GUPTA <shivanimgupta@users.noreply.github.com>
2015-03-01 22:30:29 -0500

    Create Database Queries Version 1.sql

---
+++ Database Queries Version 1.sql
@@ -0,0 +1,9 @@
+select * from station_details;
+
+select Alerts from alerts_details where Station_ID = 'station ID';
+
+select Comments from comments_details where Alert_ID = 'alert ID';
+
+select * from line_details;
+
+ 
